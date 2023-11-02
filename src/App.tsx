4c9a680ac60b1e5114af34cc18cd5fe6e0c8c474
Vadym <124910250+plesh9@users.noreply.github.com>
2023-11-03 00:08:13 +0200

    refactor

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,6 @@
-import React from 'react';
-import logo from './logo.svg';
-import { Counter } from './features/counter/Counter';
-import './App.scss';
 import { Header } from './components/Header';
+
+import './App.scss';
 
 function App() {
   return (
--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,30 +1,36 @@
 import { FC } from "react";
-import './Header.scss';
+
 import logo_1 from '../../pages/logo_1.png';
+
+import './Header.scss';
 
 export const Header: FC = () => {
     const headerButtons = [
-        {title: "ГОЛОВНА", href: ""},
-        {title: "ОСВІТНІЙ ПРОЦЕСС", href: ""},
-        {title: "МЕТОДИЧНА РОБОТА", href: ""},
-        {title: "СТУДЕНТУ", href: ""},
-        {title: "АБІТУРІЄНТУ", href: ""}
+        { title: "ГОЛОВНА", href: "" },
+        { title: "ОСВІТНІЙ ПРОЦЕСС", href: "" },
+        { title: "МЕТОДИЧНА РОБОТА", href: "" },
+        { title: "СТУДЕНТУ", href: "" },
+        { title: "АБІТУРІЄНТУ", href: "" }
     ]
-    return(
+
+    return (
         <header className="header">
             <div className="header__container">
                 <div className="header__logo">
-                    <img src={logo_1} className="header__logo_1" />
+                    <img src={logo_1} className="header__logo_1" alt="logo" />
                 </div>
                 <nav className="header__navigation">
                     <ul className="header__catalog__of__buttons">
                         {
-                            headerButtons.map((element) =>
-                                <li className="header__li"><a href={element.href} className="header__button">{element.title}</a></li>
-                            )}
+                            headerButtons.map((element) => (
+                                <li className="header__li">
+                                    <a href={element.href} className="header__button">{element.title}</a>
+                                </li>
+                            ))
+                        }
                     </ul>
                 </nav>
             </div>
         </header>
-    ) 
+    )
 }
